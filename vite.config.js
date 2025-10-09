6310396eff28e57565dc8ffe8d5041d815da0b64
Abdul Hanan <webbdevabdul@gmail.com>
2025-10-09 10:28:21 +0500

    Configure Vite server settings for external access and specify allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // allow external access (not just localhost)
+    port: 5173, // your app port
+    allowedHosts: ['builder.flipthatpdf.site'], // ✅ allow your domain
+  },
 })
